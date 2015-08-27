9c0e22678a016c6616b65b56bb0ae35110643935
Damon Rousis <damon.rousis@gmail.com>
2015-08-26 22:09:52 -0500

    removed service group from master branch

--- example.py
+++ example.py
@@ -12,11 +12,3 @@ print
 print 'Example 3 : ', elev.query([{'latitude':24, 'longitude':30}, {'latitude':25, 'longitude':30}])
 print
 print 'Example 4 : ', elev.query('latitude >= 24 and latitude <= 25 and longitude >= 30 and longitude < 31')
-
-elev1 = Service('google-elevation-api1')
-
-mygroup = ServiceGroup(elev, elev1)
-mygroup.link('x', elev.latitude, elev1.lat)
-mygroup.link('y', elev.longitude, elev1.long)
-
-mygroup.query('x=0 and y=0')
