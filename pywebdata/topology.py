4694362c085ed3967b8f4c25144fe313883f6185
Young Lee <younglee.phd@gmail.com>
2016-01-17 20:32:38 -0600

    Grow the params in a topology as each service is executed

--- pywebdata/topology.py
+++ pywebdata/topology.py
@@ -9,16 +9,16 @@ class Topology(object):
     def connect(self, u, v):
         self._graph.add_edge(u, v)
 
-    def read_input(self, d):
-        """Store the input dictionary d to a member variable"""
-        self._input = d
+    def read_input(self, input_dict):
+        """Creates a copy of the input dictionary to the topology"""
+        self._params = input_dict.copy()
 
     def run(self):
         """Checks if the topology is cyclic. If so, do not run the topology and inform the user."""
         search.initialize(self._graph)
         search.topological_sort(self._graph)
-        input_dict = self._input
         for service in reversed(search.processed_stack):
             # Note [0] at the end. Temporarily run just one query.
-            input_dict = service.query(**input_dict)[0]
-            service.update_parameters(**input_dict)
+            output = service.query(**self._params)[0]
+            service.update_parameters(**output)
+            self._params.update(output)
